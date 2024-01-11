6324af3d612ee3942b88e8de020f6db559b0ded8
HyunsooZo <bzhs1992@icloud.com>
2024-01-11 21:11:20 +0900

    chore: 프로젝트 초기 설정

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<main>
+		<div>
+			<input type="text" placeholder="" />
+			<input type="password" placeholder="" />
+			<button>로그인</button>
+			<a href="#">회원가입</a>
+			<a href="#">비밀번호 찾기</a>
+			<a href="#">아이디 찾기</a>
+		</div>
+	</main>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
