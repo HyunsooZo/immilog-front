345f3083b2035bc957d851600b4677b6f5c922a5
HyunsooZo <bzhs1992@icloud.com>
2024-01-22 21:21:07 +0900

    feat: modal sample 추가

--- src/views/TempView.vue
+++ src/views/TempView.vue
@@ -2,6 +2,23 @@
 	<div class="content">
 		<button class="button button--search" role="link">검색</button>
 	</div>
+	<div class="modal" tabindex="-1" role="dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">Modal title</h5>
+				<button type="button" class="button-util-icon close" data-dismiss="modal" aria-label="Close">닫기</button>
+			</div>
+			<div class="modal-body">
+				<p>내용</p>
+			</div>
+			<div class="modal-footer">
+				<div class="button-wrap">
+					<button type="button" class="button button--cancle" data-dismiss="modal">취소</button>
+					<button type="button" class="button button--positive">확인</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup></script>
