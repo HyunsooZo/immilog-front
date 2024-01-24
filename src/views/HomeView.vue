c7a996f6ea261369885f8c1a91d8dd79e20532d2
HyunsooZo <bzhs1992@icloud.com>
2024-01-24 21:31:29 +0900

    feat: 화면추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,43 +18,49 @@
 		<div class="container">
 			<!-- tab button -->
 			<div>탭 구분</div>
-			<!--  -->
-			<div class="category-wrap">
-				<em class="blind">카테고리</em>
-				<div class="topic">
-					<select size="1">
-						<option value="전체">전체</option>
-						<option value="내지역">내지역</option>
-						<option value="구인구직">구인구직</option>
-					</select>
-				</div>
-			</div>
-			<!-- 목록 -->
-			<div class="list-wrap">
-				<h3>
-					<i class="ico"></i>
-					<span>게시판</span>
-				</h3>
-				<div class="article">
-					<p class="ctg-wrap">
-						<a href="/">카테고리명</a>
-					</p>
-					<p class="title-wrap">
-						<a href="/" class="title">글제목</a>
-					</p>
-					<div class="info-wrap">
-						<a class="ㅔㅍ"><i class="blind">조회수</i>10</a>
-						<a class="like"><i class="blind">좋아요</i>10</a>
-						<a href="/" class="cmt"><i class="blind">댓글</i>10</a>
-						<p class="item-fnc">
-							<a href="/" class="past"><i class="blind">작성시간</i>10</a>
-							<a href="/" class="mark"><i class="blind">북마크</i>10</a>
-						</p>
-					</div>
-				</div>
-			</div>
-			<!-- //.topic-list -->
 		</div>
+		
+		<!--  -->
+		<div class="category-wrap">
+			<em class="blind">카테고리</em>
+			<div class="category__list">
+				<button type="button" class="button--select">전체</button>
+			</div>
+			<div class="sort__list">
+				<button type="button" class="button--select sort">최신순</button>
+			</div>
+		</div>
+
+		<!-- 목록 -->
+		<div class="list-wrap">
+			<p class="list__title">
+				<span>게시판이름</span>
+			</p>
+			<div class="item">
+				<p class="ctg__wrap">
+					<a href="/">카테고리명</a>
+				</p>
+				<p class="text__wrap">
+					<a href="/" class="text__item">
+						<p class="title">글제목</p>
+						<p class="text">내용미리보기</p>
+					</a>
+				</p>
+				<div class="info__wrap">
+					<p class="item-fnc">
+						<a href="/" class="pv"><i class="blind">조회수</i>10</a>
+						<a href="/" class="like"><i class="blind">좋아요</i>10</a>
+						<a href="/" class="cmt"><i class="blind">댓글</i>10</a>
+					</p>
+					<p class="item-fnc">
+						<a href="/" class="past"><i class="blind">작성시간</i>10</a>
+						<a href="/" class="mark"><i class="blind">북마크</i>10</a>
+					</p>
+				</div>
+			</div>
+			<!-- //.item -->
+		</div>
+
 	</div>
 
 	<!-- footer -->
@@ -104,6 +110,28 @@
 				</a>
 			</li>
 		</ul>
+	</div>
+
+	<!-- selectdialog -->
+	<div class="modal-container select--dialog" style="display: none;">
+		<div class="modal" tabindex="-1" role="dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<p class="modal-title">카테고리 선택</p>
+					<button type="button" class="button-icon button--close" role="link">
+						<span>닫기</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<ul class="list-wrap">
+						<li class="item"><a href="/">전체</a></li>
+						<li class="item"><a href="/">인기글</a></li>
+						<li class="item"><a href="/">내지역</a></li>
+						<li class="item"><a href="/">구인구직</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
