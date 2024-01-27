f690508571dd2ea3fdc0ef14c578d29a80e3fd11
HyunsooZo <bzhs1992@icloud.com>
2024-01-27 23:58:27 +0900

    fix: 알럿 버그 수정

--- src/components/modal/CustomAlert.vue
+++ src/components/modal/CustomAlert.vue
@@ -1,31 +1,25 @@
 <template>
-	<Transition>
-		<div v-if="alertValue" class="modal-container">
-			<div class="modal" tabindex="-1" role="dialog">
-				<div class="modal-content" @click.stop>
-					<div class="modal-body">
-						<p>{{ alertText }}</p>
-					</div>
-					<div class="modal-footer">
-						<div class="button-wrap">
-							<button
-								type="button"
-								class="button button--positive"
-								@click="closeModal"
-							>
-								확인
-							</button>
-						</div>
-					</div>
+	<div class="modal" tabindex="-1" role="dialog">
+		<div class="modal-content" @click.stop>
+			<div class="modal-body">
+				<p v-html="alertText"></p>
+			</div>
+			<div class="modal-footer">
+				<div class="button-wrap">
+					<button
+						type="button"
+						class="button button--positive"
+						@click="closeModal"
+					>
+						확인
+					</button>
 				</div>
 			</div>
 		</div>
-	</Transition>
+	</div>
 </template>
 
 <script setup>
-import { defineProps, defineEmits } from 'vue';
-
 const { alertValue, alertText } = defineProps(['alertValue', 'alertText']);
 
 const emits = defineEmits(['update:alertValue']);
