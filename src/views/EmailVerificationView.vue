3ea63a634f19b87a521ad5d2e7cf71873d393e28
HyunsooZo <bzhs1992@icloud.com>
2024-01-28 04:53:07 +0900

    feat: email

--- src/views/EmailVerificationView.vue
+++ src/views/EmailVerificationView.vue
@@ -1,5 +1,21 @@
 <template>
-	<div>이메일인증하삼~</div>
+	<div class="content">
+		<div class="msg-wrap">
+			<i class="item__icon item__icon--check"></i>
+			<p class="item__msg"><em>회원가입이 완료</em> 되었습니다.</p>
+			<p class="item__description">가입한 이메일에서 인증 확인 후 완료 됩니다.</p>
+		</div>
+		<!-- test -->
+		<!-- <svg class="svg--logo" viewBox="0 0 1000 300">
+			<text x="50%" y="50%" dy="40px" text-anchor="middle">KoMeet</text>
+		</svg>
+		<svg class="svg--circle" viewBox="0 0 168 168" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M163 84C163 127.63 127.63 163 84 163C40.3695 163 5 127.63 5 84C5 40.3695 40.3695 5 84 5C127.63 5 163 40.3695 163 84Z"
+				stroke="#666" stroke-width="4" />
+		</svg> -->
+
+	</div>
 </template>
 
 <script setup></script>
