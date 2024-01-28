ccac7e7dcc39aeeb7351f44c79f16ef85a500e52
HyunsooZo <bzhs1992@icloud.com>
2024-01-28 23:53:14 +0900

    feat: splash test

--- src/views/JobBoardView.vue
+++ src/views/JobBoardView.vue
@@ -1,5 +1,17 @@
 <template>
-	<div></div>
+	<div class="content">
+		<div class="splash">
+			<div class="svg--wrap">
+				<link href="https://fonts.googleapis.com/css?family=Leckerli+One" rel="stylesheet">
+				<svg class="svg--logo" viewBox="0 0 350 100">
+					<text x="50%" y="50%" dy="0" text-anchor="middle">
+						KoMeet
+						<!-- <tspan dy="0" fill="#dfdfdf" font-size="30">Komeet</tspan> -->
+					</text>
+				</svg>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup></script>
