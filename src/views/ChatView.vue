7f92ae733d98971d85c28e3512080a5e03bba4b1
HyunsooZo <bzhs1992@icloud.com>
2024-01-29 00:57:14 +0900

    feat: splash

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,6 +1,18 @@
 <template>
-	<TheHeader />
-	<div></div>
+	<!-- <TheHeader /> -->
+	<div class="content">
+		<div class="splash color--bg"><!-- //.color--bg -->
+			<div class="svg--wrap">
+				<link href="https://fonts.googleapis.com/css?family=Leckerli+One" rel="stylesheet">
+				<svg class="svg--logo" viewBox="0 0 350 100">
+					<text x="50%" y="50%" dy="0" text-anchor="middle">
+						KoMeet
+						<!-- <tspan dy="0" fill="#dfdfdf" font-size="30">Komeet</tspan> -->
+					</text>
+				</svg>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
