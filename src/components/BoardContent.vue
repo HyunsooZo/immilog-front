16663fce314733ae65769828c02cc9d49bd6dc8b
HyunsooZo <bzhs1992@icloud.com>
2024-01-29 22:20:01 +0900

    feat: 목록 데이터 불러오도록 수정

--- src/components/BoardContent.vue
+++ src/components/BoardContent.vue
@@ -2,7 +2,7 @@
 	<div class="item">
 		<div class="info__wrap">
 			<div class="item__pic">
-				<img src="" alt="" />
+				<img :src="post.userProfileUrl" alt="" />
 			</div>
 			<div class="item-fnc">
 				<div class="list__item">
