e293857b1993044e356cf33f3d9cf6687fa1554a
HyunsooZo <bzhs1992@icloud.com>
2024-02-13 05:10:09 +0900

    feat: comment

--- src/components/ReplyWrite.vue
+++ src/components/ReplyWrite.vue
@@ -26,10 +26,6 @@
 						<textarea v-model="textareaRef" class="text__area" name="content" autocomplete="off"
 							placeholder="댓글을 입력해주세요. 다른 사용자로부터 일정 수 이상의 신고를 받는 경우 글이 자동으로 숨김처리 될 수 있습니다."
 							data-autosuggest_is-input="true" @input="adjustTextareaHeight" rows="2"></textarea>
-
-						<!-- <textarea v-model="content" class="text__area" name="content" autocomplete="off"
-							placeholder="댓글을 입력해주세요. 다른 사용자로부터 일정 수 이상의 신고를 받는 경우 글이 자동으로 숨김처리 될 수 있습니다."
-							data-autosuggest_is-input="true" ref="textareaRef" @input="adjustTextareaHeight" rows="2"></textarea> -->
 					</div>
 					<!-- <div class="util__wrap">
 						<div class="item__fnc">
