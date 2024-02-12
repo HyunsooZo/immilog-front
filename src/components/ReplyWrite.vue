24df805ae99ce25cf81190bade11d903566affb6
HyunsooZo <bzhs1992@icloud.com>
2024-02-12 23:38:33 +0900

    feat: 댓글 데이터 불러오는 기능 추가

--- src/components/ReplyWrite.vue
+++ src/components/ReplyWrite.vue
@@ -1,33 +1,57 @@
 <template>
 	<!-- replydialog -->
-	<div class="modal reply--dialog" tabindex="-1" role="dialog" @click.self="closeDialog">
+	<div
+		class="modal reply--dialog"
+		tabindex="-1"
+		role="dialog"
+		@click.self="closeDialog"
+	>
 		<div class="modal-content">
 			<div class="modal-header">
 				<p class="modal-title">{{ title }}</p>
-				<button type="button" class="button-icon button--close" role="link" @click="closeDialog">
+				<button
+					type="button"
+					class="button-icon button--close"
+					role="link"
+					@click="closeDialog"
+				>
 					<span class="blind">닫기</span>
 				</button>
 			</div>
 			<div class="modal-body">
 				<div class="post-wrap">
 					<div class="post__wrap">
-						<textarea class="text__area" name="content" autocomplete="off"
-							placeholder="다른 사용자로부터 일정 수 이상의 신고를 받는 경우 글이 자동으로 숨김처리 될 수 있습니다." data-autosuggest_is-input="true"
-							ref="textareaRef" @input="adjustTextareaHeight" rows="3"></textarea>
+						<textarea
+							v-model="textareaRef"
+							class="text__area"
+							name="content"
+							autocomplete="off"
+							placeholder="다른 사용자로부터 일정 수 이상의 신고를 받는 경우 글이 자동으로 숨김처리 될 수 있습니다."
+							data-autosuggest_is-input="true"
+							@input="adjustTextareaHeight"
+							rows="3"
+						></textarea>
 					</div>
 					<div class="util__wrap">
 						<!-- 사진추가 -->
 						<div class="item__fnc">
 							<div class="input__wrap input__attachments">
 								<div class="input__file">
-									<input type="file" id="file-upload" multiple="multiple"
-										accept="image/jpeg, image/png, image/gif, image/jpg, image/tiff" @change="previewImage" />
+									<input
+										type="file"
+										id="file-upload"
+										multiple="multiple"
+										accept="image/jpeg, image/png, image/gif, image/jpg, image/tiff"
+										@change="previewImage"
+									/>
 									<label for="file-upload" class="button-icon__s" role="button">
 										<svg viewBox="0 0 16 16">
 											<path
-												d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" />
+												d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"
+											/>
 											<path
-												d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
+												d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"
+											/>
 										</svg>
 										<span class="blind">사진 선택</span>
 									</label>
@@ -36,13 +60,25 @@
 						</div>
 						<!-- 댓글등록 -->
 						<div class="item__fnc right">
-							<button type="button" class="button-icon__s button--post_upload">
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-									class="bi bi-check2-square" viewBox="0 0 16 16">
+							<button
+								type="button"
+								class="button-icon__s button--post_upload"
+								@click="commentApi"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-check2-square"
+									viewBox="0 0 16 16"
+								>
 									<path
-										d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z" />
+										d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"
+									/>
 									<path
-										d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+										d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"
+									/>
 								</svg>
 								<span>등록</span>
 							</button>
@@ -52,14 +88,43 @@
 			</div>
 		</div>
 	</div>
+	<CustomAlert
+		v-if="alertValue"
+		:alertValue="alertValue"
+		:alertText="alertText"
+		@update:alertValue="closeAlert"
+	/>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import useAxios from '@/composables/useAxios';
+import CustomAlert from './modal/CustomAlert.vue';
+
+const { sendRequest } = useAxios();
 
 const title = '댓글쓰기';
 
+const alertValue = ref(false);
+const alertText = ref('');
+
 const emit = defineEmits(['close']);
+
+const props = defineProps({
+	postSeq: {
+		type: Number,
+		required: false,
+	},
+	commentSeq: {
+		type: Number,
+		required: false,
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+});
+
 const closeDialog = () => {
 	emit('close');
 };
@@ -70,5 +135,36 @@ const adjustTextareaHeight = () => {
 	const textarea = textareaRef.value;
 	textarea.style.height = 'auto';
 	textarea.style.height = `${textarea.scrollHeight}px`;
+};
+
+const openAlert = content => {
+	alertValue.value = true;
+	alertText.value = content;
+};
+
+const closeAlert = () => {
+	alertValue.value = false;
+};
+
+const commentApi = async () => {
+	// 댓글 등록 api
+	try {
+		await sendRequest(
+			'post',
+			`/comments/posts/${props.postSeq}`,
+			{
+				headers: {
+					contentType: 'application/json',
+					Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+				},
+			},
+			{
+				content: textareaRef.value,
+			},
+		);
+	} catch (error) {
+		openAlert('서버와의 통신에 실패했습니다.');
+		console.log(error);
+	}
 };
 </script>
--- src/views/BoardDetailView.vue
+++ src/views/BoardDetailView.vue
@@ -36,16 +36,26 @@
 				</div>
 				<!-- file preview -->
 				<div class="attachments__wrap" v-if="post.attachments.length > 0">
-					<div class="attachments__item" v-for="(image, index) in post.attachments" :key="index">
+					<div
+						class="attachments__item"
+						v-for="(image, index) in post.attachments"
+						:key="index"
+					>
 						<div class="item__display">
 							<img :src="image" alt="preview" />
 						</div>
-					</div><!-- //loop -->
+					</div>
+					<!-- //loop -->
 				</div>
 				<div class="tag__wrap">
 					<div class="tag__inner">
 						<div class="tag__item">
-							<button v-for="tag in post.tags" :key="tag" type="button" class="button button--hash">
+							<button
+								v-for="tag in post.tags"
+								:key="tag"
+								type="button"
+								class="button button--hash"
+							>
 								<em>{{ tag }}</em>
 							</button>
 						</div>
@@ -53,7 +63,12 @@
 				</div>
 				<div class="util__wrap">
 					<div class="item__fnc">
-						<button type="button" class="list__item_button like" :class="{ active: isLiked }" @click="likeApi">
+						<button
+							type="button"
+							class="list__item_button like"
+							:class="{ active: isLiked }"
+							@click="likeApi"
+						>
 							<i class="blind">좋아요</i>
 							<span class="item__count">{{ post.likeCount }}</span>
 						</button>
@@ -73,7 +88,8 @@
 							<i class="blind">공유하기</i>
 						</button>
 						<button type="button" class="list__item_button more">
-							<i class="blind">더보기</i><!-- //차단하기, 대화하기.. -->
+							<i class="blind">더보기</i
+							><!-- //차단하기, 대화하기.. -->
 						</button>
 					</div>
 				</div>
@@ -84,21 +100,32 @@
 		<!-- 댓글 기능버튼 -->
 		<div class="fnc-wrap">
 			<div class="button__list">
-				<button type="button" class="button-icon__s button--post" @click="openReplyWrite">
+				<button
+					type="button"
+					class="button-icon__s button--post"
+					@click="openReplyWrite"
+				>
 					<svg viewBox="0 0 16 16">
 						<path
-							d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+							d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
+						/>
 						<path
-							d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+							d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+						/>
 					</svg>
 					<span>댓글쓰기</span>
 				</button>
 			</div>
 			<div class="sort__list">
-				<button type="button" class="button-icon__s last-reply" @click="goToDown">
+				<button
+					type="button"
+					class="button-icon__s last-reply"
+					@click="goToDown"
+				>
 					<svg viewBox="0 0 16 16">
 						<path
-							d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z" />
+							d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
+						/>
 					</svg>
 					<span>마지막 댓글로 이동</span>
 				</button>
@@ -106,16 +133,16 @@
 		</div>
 		<!-- 댓글 -->
 		<div class="list-wrap reply">
-			<div class="item item--blind">
+			<div class="item" v-for="comment in post.comments" :key="comment.seq">
 				<!-- //댓글 신고로 숨김처리 .item--blind -->
 				<!-- 댓글 신고로 숨김처리 시 대체 텍스트 -->
-				<div class="blind__text">신고에 의해 숨김처리 되었습니다.</div>
+				<!-- <div class="blind__text">신고에 의해 숨김처리 되었습니다.</div> -->
 				<div class="info__wrap">
 					<div class="item__fnc">
 						<div class="list__item">
 							<button type="button" class="list__item_button user">
 								<em>댓글작성자</em>
-								<strong>닉네임</strong>
+								<strong>{{ comment.user.nickName }}</strong>
 							</button>
 						</div>
 					</div>
@@ -123,7 +150,7 @@
 				<div class="text__wrap">
 					<div class="list__item">
 						<div class="text__item">
-							<p class="text">댓글내용</p>
+							<p class="text">{{ comment.content }}</p>
 						</div>
 					</div>
 				</div>
@@ -132,19 +159,22 @@
 						<button type="button" class="list__item_button like active">
 							<!-- //활성화 .active -->
 							<i class="blind">좋아요</i>
-							<span class="item__count">10</span>
+							<span class="item__count">{{ comment.upVotes }}</span>
 						</button>
 						<button type="button" class="list__item cmt">
-							<span class="item__count">대댓글</span>
+							<span class="item__count">{{ comment.replies.length }}</span>
 						</button>
 						<p class="list__item past">
 							<i class="blind">작성시간</i>
-							<span class="item__count">10시간 전</span>
+							<span class="item__count">{{
+								timeCalculation(comment.createdAt)
+							}}</span>
 						</p>
 					</div>
 					<div class="item__fnc">
 						<button type="button" class="list__item_button more">
-							<i class="blind">더보기</i><!-- //차단하기, 대화하기.. -->
+							<i class="blind">더보기</i
+							><!-- //차단하기, 대화하기.. -->
 						</button>
 					</div>
 				</div>
@@ -156,7 +186,10 @@
 					<div class="info__wrap">
 						<div class="item__fnc">
 							<div class="list__item">
-								<button type="button" class="list__item_button user user--author">
+								<button
+									type="button"
+									class="list__item_button user user--author"
+								>
 									<!-- //원글작성자 댓글 .user--author -->
 									<em>원글작성자 대댓글</em>
 									<strong>원글작성자 닉네임</strong>
@@ -185,7 +218,8 @@
 						</div>
 						<div class="item__fnc">
 							<button type="button" class="list__item_button more">
-								<i class="blind">더보기</i><!-- //차단하기, 대화하기.. -->
+								<i class="blind">더보기</i
+								><!-- //차단하기, 대화하기.. -->
 							</button>
 						</div>
 					</div>
@@ -223,7 +257,8 @@
 						</div>
 						<div class="item__fnc">
 							<button type="button" class="list__item_button more">
-								<i class="blind">더보기</i><!-- //차단하기, 대화하기.. -->
+								<i class="blind">더보기</i
+								><!-- //차단하기, 대화하기.. -->
 							</button>
 						</div>
 					</div>
@@ -240,8 +275,13 @@
 	</div>
 	<!-- <SelectDialog v-if="isSortingSelectClicked" :title="selectTitle" :list="selectList" @close="closeSelect"
 		@select:value="selectedValue" /> -->
-	<ReplyWrite v-if="isReplyWriteClicked" :title="selectTitle" :list="selectList" @close="closeSelect"
-		@select:value="selectedValue" />
+	<ReplyWrite
+		v-if="isReplyWriteClicked"
+		:postSeq="post.seq"
+		:type="'post'"
+		@close="closeSelect"
+		@select:value="selectedValue"
+	/>
 </template>
 
 <script setup>
@@ -265,6 +305,7 @@ const { sendRequest } = useAxios();
 const router = useRouter();
 const route = useRoute();
 const postSeq = route.params.postId;
+
 const post = ref({
 	seq: 0,
 	title: '',
