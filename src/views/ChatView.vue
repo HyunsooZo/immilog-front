6247a0fa563e1f2cb53a9b5df14e3dc0e86d0c1e
HyunsooZo <bzhs1992@icloud.com>
2024-02-13 21:21:55 +0900

    feat: chatview

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,7 +1,70 @@
 <template>
 	<div class="content">
-
+		<div class="list-wrap chat">
+			<button type="button" class="item">
+				<div class="info__wrap">
+					<div class="item__pic pic--default">
+						<!-- <img src="" alt="" /> -->
+					</div>
+					<div class="item__fnc">
+						<div class="list__item">
+							<div class="list__item user">
+								<em>post.region</em>
+								<strong>post.userNickName</strong>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="text__wrap">
+					<div class="list__item">
+						<div class="text__item">
+							<p class="text">chat.content</p>
+						</div>
+					</div>
+				</div>
+				<div class="util__wrap">
+					<div class="item__fnc">
+						<p class="list__item past">
+							<i class="blind">작성시간</i>
+							<span class="item__count">chat.createdAt</span>
+						</p>
+					</div>
+				</div>
+			</button>
+			<button type="button" class="item">
+				<div class="info__wrap">
+					<div class="item__pic pic--default">
+						<!-- <img src="" alt="" /> -->
+					</div>
+					<div class="item__fnc">
+						<div class="list__item">
+							<div class="list__item user">
+								<em>post.region</em>
+								<strong>post.userNickName</strong>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="text__wrap">
+					<div class="list__item">
+						<div class="text__item">
+							<p class="text">chat.content</p>
+						</div>
+					</div>
+				</div>
+				<div class="util__wrap">
+					<div class="item__fnc">
+						<p class="list__item past">
+							<i class="blind">작성시간</i>
+							<span class="item__count">chat.createdAt</span>
+						</p>
+					</div>
+				</div>
+			</button>
+		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+
+</script>
