5436f0b19366c2e444562e33092a8e1a5fd6c26a
HyunsooZo <bzhs1992@icloud.com>
2024-02-13 21:44:14 +0900

    feat: chatview

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,66 +1,41 @@
 <template>
 	<div class="content">
 		<div class="list-wrap chat">
-			<button type="button" class="item">
-				<div class="info__wrap">
-					<div class="item__pic pic--default">
-						<!-- <img src="" alt="" /> -->
-					</div>
-					<div class="item__fnc">
-						<div class="list__item">
+			<div class="item">
+				<button type="button" class="list__item_button">
+					<div class="info__wrap">
+						<div class="item__pic pic--default">
+							<!-- <img src="" alt="" /> -->
+						</div>
+						<div class="item__fnc">
 							<div class="list__item user">
 								<em>post.region</em>
 								<strong>post.userNickName</strong>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="text__wrap">
-					<div class="list__item">
-						<div class="text__item">
-							<p class="text">chat.content</p>
-						</div>
-					</div>
-				</div>
-				<div class="util__wrap">
-					<div class="item__fnc">
-						<p class="list__item past">
-							<i class="blind">작성시간</i>
-							<span class="item__count">chat.createdAt</span>
-						</p>
-					</div>
-				</div>
-			</button>
-			<button type="button" class="item">
-				<div class="info__wrap">
-					<div class="item__pic pic--default">
-						<!-- <img src="" alt="" /> -->
-					</div>
-					<div class="item__fnc">
+					<div class="text__wrap">
 						<div class="list__item">
-							<div class="list__item user">
-								<em>post.region</em>
-								<strong>post.userNickName</strong>
+							<div class="text__item">
+								<p class="text">chat.content</p>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="text__wrap">
-					<div class="list__item">
-						<div class="text__item">
-							<p class="text">chat.content</p>
+					<div class="util__wrap">
+						<div class="item__fnc">
+							<p class="list__item past">
+								<i class="blind">작성시간</i>
+								<span class="item__count">chat.createdAt</span>
+							</p>
 						</div>
 					</div>
+				</button>
+				<div class="item__fnc">
+					<button type="button" class="list__item_button more">
+						<i class="blind">더보기</i><!-- //신고, 나가기 -->
+					</button>
 				</div>
-				<div class="util__wrap">
-					<div class="item__fnc">
-						<p class="list__item past">
-							<i class="blind">작성시간</i>
-							<span class="item__count">chat.createdAt</span>
-						</p>
-					</div>
-				</div>
-			</button>
+			</div>
 		</div>
 	</div>
 </template>
