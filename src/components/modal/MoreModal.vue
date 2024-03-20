b812fc2950dae99a037c9becf3f79b4445166047
HyunsooZo <bzhs1992@icloud.com>
2024-03-20 21:02:38 +0900

    feat: moremodal

---
+++ src/components/modal/MoreModal.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="modal" tabindex="-1" role="dialog">
+		<div class="modal-content" @click.stop>
+			<div class="modal-header">
+				<p class="modal-title">{{ title }}</p>
+				<button type="button" class="button-icon button--close" role="link" @click="closeModal">
+					<i class="blind">닫기</i>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="list-wrap">
+					<ul>
+						<li v-for="(item, index) in list" :key="index" class="item">
+							<button type="button" class="button" @click="">
+								<span>{{ item.name }}</span>
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const closeModal = () => {
+	emits('update:alertValue', false);
+};
+</script>
