9e38b4ac574fe5ff5c0eb9b9af9e84c2439d97e5
hyunsooZo <bzhs1992@icloud.com>
2024-07-05 13:34:47 +0900

    feat: 언어설정 시 전체 언어 바뀌도록 수정

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -1,26 +1,3 @@
-<template>
-	<!-- footer 영역 -->
-	<div class="footer">
-		<ul class="util-item-wrap">
-			<!-- 메뉴 아이템을 반복적으로 렌더링 -->
-			<li v-for="(item, index) in menuItems" :key="index" class="util__item">
-				<button type="button" :class="{
-				active: activeItem === index,
-				item: true,
-				[item.styleClass]: true,
-			}" @click="onMenuItemClick(index)">
-					<!-- SVG 아이콘 렌더링 -->
-					<svg :width="16" :height="16" :viewBox="iconViewBox" aria-hidden="true">
-						<!-- 다중 패스 지원 -->
-						<path v-for="(path, pathIndex) in item.paths" :key="pathIndex" :d="path" />
-					</svg>
-					<span>{{ item.label }}</span>
-				</button>
-			</li>
-		</ul>
-	</div>
-</template>
-
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
@@ -34,48 +11,40 @@ import {
 } from '@/utils/icons.ts';
 import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n();
+const { t, locale } = useI18n();
 
 const route = useRoute();
 const router = useRouter();
 const iconViewBox = '0 0 16 16'; // 모든 아이콘에 적용되는 viewBox 값
 
-// 메뉴 아이템 정의
-const menuItems = ref([
-	// 홈 아이콘
-	{
-		label: t('footer.home'),
-		styleClass: 'home',
-		paths: [homeIcon],
-	},
-	// 내지역 아이콘
-	{
-		label: t('footer.myCountry'),
-		styleClass: 'area',
-		paths: [areaIcon],
-	},
-	// 채팅 아이콘
-	{
-		label: t('footer.chat'),
-		styleClass: 'chat',
-		paths: [chatIcon.first, chatIcon.second],
-	},
-	// 구인/구직 아이콘
-	{
-		label: t('footer.job'),
-		styleClass: 'job',
-		paths: [jobIcon.first, jobIcon.second, jobIcon.third],
-	},
-	// 마이페이지 아이콘
-	{
-		label: t('footer.myPage'),
-		styleClass: 'my',
-		paths: [myPageIcon],
-	},
-]);
+interface MenuItem {
+	label: string;
+	styleClass: string;
+	paths: string[];
+}
 
-// 현재, 그리고 마지막으로 활성화된 메뉴의 인덱스
 const activeItem = ref(0);
+const menuItems = ref<MenuItem[]>([]);
+
+// 메뉴 아이템을 설정하는 함수
+const setMenuItems = () => {
+	menuItems.value = [
+		{ label: t('footer.home'), styleClass: 'home', paths: [homeIcon] },
+		{ label: t('footer.myCountry'), styleClass: 'area', paths: [areaIcon] },
+		{ label: t('footer.chat'), styleClass: 'chat', paths: [chatIcon.first, chatIcon.second] },
+		{ label: t('footer.job'), styleClass: 'job', paths: [jobIcon.first, jobIcon.second, jobIcon.third] },
+		{ label: t('footer.myPage'), styleClass: 'my', paths: [myPageIcon] },
+	];
+};
+
+// 초기 메뉴 아이템 설정
+setMenuItems();
+
+// 언어 변경 감지하여 메뉴 아이템 업데이트
+watchEffect(() => {
+	locale.value; // 언어 변경 감지
+	setMenuItems();
+});
 
 // 라우트 매핑과 메뉴 아이템 클릭 핸들러
 const routeMapping: Record<number, string> = {
@@ -86,14 +55,12 @@ const routeMapping: Record<number, string> = {
 	4: '/my-page',
 };
 
-// 메뉴 아이템 클릭 핸들러
 const onMenuItemClick = (index: number) => {
 	useMenuStore().setMenu(index);
 	router.push(routeMapping[index] || '/');
 	activeItem.value = index;
 };
 
-// 라우트 경로를 메뉴 인덱스에 매핑
 const routeToIndexMapping: { [key: string]: number } = {
 	'/': 0,
 	'/board': 1,
@@ -106,9 +73,28 @@ const routeToIndexMapping: { [key: string]: number } = {
 watchEffect(() => {
 	const path = route.path;
 	const mappedIndex = routeToIndexMapping[path];
-	console.log(path);
 	if (mappedIndex !== undefined) {
 		activeItem.value = mappedIndex;
 	}
 });
 </script>
+
+<template>
+	<!-- Template 코드 -->
+	<div class="footer">
+		<ul class="util-item-wrap">
+			<li v-for="(item, index) in menuItems" :key="index" class="util__item">
+				<button type="button" :class="{
+					active: activeItem === index,
+					item: true,
+					[item.styleClass]: true,
+				}" @click="onMenuItemClick(index)">
+					<svg :width="16" :height="16" :viewBox="iconViewBox" aria-hidden="true">
+						<path v-for="(path, pathIndex) in item.paths" :key="pathIndex" :d="path" />
+					</svg>
+					<span>{{ item.label }}</span>
+				</button>
+			</li>
+		</ul>
+	</div>
+</template>
--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -1,213 +1,237 @@
 <template>
-	<div class="content">
-		<!-- 개인정보 -->
-		<div class="list-wrap _personal">
-			<div class="item">
-				<button type="button" class="list__item_button" role="link" @click="onProfileEdit">
-					<div class="info__wrap">
-						<div class="item__pic" :class="{ 'pic--default': !userInfo.userProfileUrl }">
-							<img v-if="userInfo.userProfileUrl" :src="userInfo.userProfileUrl" alt="" />
-						</div>
-						<div class="item__fnc">
-							<div class="list__item">
-								<em v-if="loginStatus">{{ t('countries.' + userInfo.userCountry) }}</em>
-								<em v-if="loginStatus">{{ userInfo.userRegion }}</em>
-							</div>
-							<div class="list__item user button-text">
-								<strong>{{ userInfo.userNickname ? userInfo.userNickname : t('myPageView.login') }} </strong>
-							</div>
-						</div>
-					</div>
-				</button>
-			</div>
-		</div>
-		<!--  -->
-		<div class="list-wrap list--link">
-			<ul v-if="loginStatus">
-				<li class="item">
-					<button type="button" class="button button-text" role="link" @click="onMyBoard">
-						<svg viewBox="0 0 16 16">
-							<path :d="myPostIcon" />
-						</svg>
-						<span>{{ t('myPageView.myPosts') }}</span>
-					</button>
-				</li>
-				<li class="item">
-					<button type="button" class="button button-text" role="link" @click="onBookmark">
-						<svg viewBox="0 0 16 16">
-							<path fill-rule="evenodd" :d="bookmarkIcon.first" />
-							<path :d="bookmarkIcon.second" />
-						</svg>
-						<span>{{ t('myPageView.bookMark') }}</span>
-					</button>
-				</li>
-			</ul>
-			<ul>
-				<li class="item" v-if="loginStatus">
-					<button type="button" class="button button-text" role="link" @click="onMenuOpen">
-						<svg viewBox="0 0 16 16">
-							<path :d="noticeIcon.first" />
-							<path :d="noticeIcon.second" />
-						</svg>
-						<span>{{ t('myPageView.notice') }}</span>
-					</button>
-				</li>
-				<li class="item">
-					<button type="button" class="button button-text" role="link" @click="openEmailForm">
-						<svg viewBox="0 0 16 16">
-							<path :d="customerCenterIcon.first" />
-							<path :d="customerCenterIcon.second" />
-						</svg>
-						<span>{{ t('myPageView.customerService') }}</span>
-					</button>
-				</li>
-				<li class="item" v-if="loginStatus">
-					<button type="button" class="button" role="link" @click="signOut">
-						<svg viewBox="0 0 16 16">
-							<path :d="logoutIcon.first" />
-							<path :d="logoutIcon.second" />
-						</svg>
-						<span>{{ t('myPageView.logout') }}</span>
-					</button>
-				</li>
-			</ul>
-			<ul>
-				<li class="item">
-					<div class="check">
-						<svg viewBox="0 0 16 16">
-							<path :d="settingIcon.first" />
-							<path :d="settingIcon.second" />
-						</svg>
-						<span>{{ t('myPageView.settings') }}</span>
-						<div class="input__item _switch-toggle">
-							<input type="checkbox" class="input__checkbox" name="setting" id="setting" />
-							<label for="setting" class="input__label"><span class="blind">{{ t('myPageView.korean') }}</span></label>
-							<!-- <label for="setting" class="input__label">{{ t('myPageView.english') }}</label> -->
-						</div>
-					</div>
-				</li>
-			</ul>
-		</div>
-		<BookMark @update:bookmarkValue="offBookMark" v-if="isBookmarkOn" />
-		<UserBoard :userSeq="userInfo.userSeq ? userInfo.userSeq : 0" @close="offUserBoard" v-if="isUserBoardOn" />
-	</div>
-	<teleport to="#modal" v-if="modalValue">
-		<ConfirmModal :modalText="modalText" @modalValue="closeModal" />
-	</teleport>
-	<NotificationModal v-if="onMenu" @close="onMenuClose" />
+  <div class="content">
+    <!-- 개인정보 -->
+    <div class="list-wrap _personal">
+      <div class="item">
+        <button type="button" class="list__item_button" role="link" @click="onProfileEdit">
+          <div class="info__wrap">
+            <div class="item__pic" :class="{ 'pic--default': !userInfo.userProfileUrl }">
+              <img v-if="userInfo.userProfileUrl" :src="userInfo.userProfileUrl" alt="" />
+            </div>
+            <div class="item__fnc">
+              <div class="list__item">
+                <em v-if="loginStatus">{{ t('countries.' + userInfo.userCountry) }}</em>
+                <em v-if="loginStatus">{{ userInfo.userRegion }}</em>
+              </div>
+              <div class="list__item user button-text">
+                <strong
+                  >{{ userInfo.userNickname ? userInfo.userNickname : t('myPageView.login') }}
+                </strong>
+              </div>
+            </div>
+          </div>
+        </button>
+      </div>
+    </div>
+    <!--  -->
+    <div class="list-wrap list--link">
+      <ul v-if="loginStatus">
+        <li class="item">
+          <button type="button" class="button button-text" role="link" @click="onMyBoard">
+            <svg viewBox="0 0 16 16">
+              <path :d="myPostIcon" />
+            </svg>
+            <span>{{ t('myPageView.myPosts') }}</span>
+          </button>
+        </li>
+        <li class="item">
+          <button type="button" class="button button-text" role="link" @click="onBookmark">
+            <svg viewBox="0 0 16 16">
+              <path fill-rule="evenodd" :d="bookmarkIcon.first" />
+              <path :d="bookmarkIcon.second" />
+            </svg>
+            <span>{{ t('myPageView.bookMark') }}</span>
+          </button>
+        </li>
+      </ul>
+      <ul>
+        <li class="item" v-if="loginStatus">
+          <button type="button" class="button button-text" role="link" @click="onMenuOpen">
+            <svg viewBox="0 0 16 16">
+              <path :d="noticeIcon.first" />
+              <path :d="noticeIcon.second" />
+            </svg>
+            <span>{{ t('myPageView.notice') }}</span>
+          </button>
+        </li>
+        <li class="item">
+          <button type="button" class="button button-text" role="link" @click="openEmailForm">
+            <svg viewBox="0 0 16 16">
+              <path :d="customerCenterIcon.first" />
+              <path :d="customerCenterIcon.second" />
+            </svg>
+            <span>{{ t('myPageView.customerService') }}</span>
+          </button>
+        </li>
+        <li class="item" v-if="loginStatus">
+          <button type="button" class="button" role="link" @click="signOut">
+            <svg viewBox="0 0 16 16">
+              <path :d="logoutIcon.first" />
+              <path :d="logoutIcon.second" />
+            </svg>
+            <span>{{ t('myPageView.logout') }}</span>
+          </button>
+        </li>
+      </ul>
+      <ul>
+        <li class="item">
+          <div class="check">
+            <svg viewBox="0 0 16 16">
+              <path :d="settingIcon.first" />
+              <path :d="settingIcon.second" />
+            </svg>
+            <span>{{ t('myPageView.settings') }}</span>
+            <div class="input__item _switch-toggle">
+              <input
+                type="checkbox"
+                class="input__checkbox"
+                name="setting"
+                id="setting"
+                v-model="isKoreanChecked"
+              />
+              <label for="setting" class="input__label"
+                ><span class="blind">{{ t('myPageView.korean') }}</span></label
+              >
+              <!-- <label for="setting" lcass="input__label">{{ t('myPageView.english') }}</label> -->
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <BookMark @update:bookmarkValue="offBookMark" v-if="isBookmarkOn" />
+    <UserBoard
+      :userSeq="userInfo.userSeq ? userInfo.userSeq : 0"
+      @close="offUserBoard"
+      v-if="isUserBoardOn"
+    />
+  </div>
+  <teleport to="#modal" v-if="modalValue">
+    <ConfirmModal :modalText="modalText" @modalValue="closeModal" />
+  </teleport>
+  <NotificationModal v-if="onMenu" @close="onMenuClose" />
 </template>
 
 <script setup lang="ts">
-import BookMark from '@/components/board/BookMark.vue';
-import UserBoard from '@/components/board/UserBoard.vue';
-import ConfirmModal from '@/components/modal/ConfirmModal.vue';
-import { useUserInfoStore } from '@/stores/userInfo.ts';
-import { onMounted, ref } from 'vue';
-import { useRouter } from 'vue-router';
+import BookMark from '@/components/board/BookMark.vue'
+import UserBoard from '@/components/board/UserBoard.vue'
+import ConfirmModal from '@/components/modal/ConfirmModal.vue'
+import { useUserInfoStore } from '@/stores/userInfo.ts'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
 import {
-	myPostIcon,
-	bookmarkIcon,
-	settingIcon,
-	noticeIcon,
-	customerCenterIcon,
-	logoutIcon,
-} from '@/utils/icons.ts';
-import { useI18n } from 'vue-i18n';
-import NotificationModal from '@/components/notification/NotificationModal.vue';
+  myPostIcon,
+  bookmarkIcon,
+  settingIcon,
+  noticeIcon,
+  customerCenterIcon,
+  logoutIcon
+} from '@/utils/icons.ts'
+import { useI18n } from 'vue-i18n'
+import NotificationModal from '@/components/notification/NotificationModal.vue'
+import { watchEffect } from 'vue'
 
-const { t } = useI18n();
-const router = useRouter();
-const userInfo = useUserInfoStore();
-const loginStatus = ref(false);
+const { t, locale } = useI18n();
+const router = useRouter()
+const userInfo = useUserInfoStore()
+const loginStatus = ref(false)
+const isKoreanChecked = ref(true)
 
-// modal open/close 시 body 컨트롤
-const modalOpenClass = () => {
-	document.body.classList.add('inactive');
-};
-const modalCloseClass = () => {
-	document.body.classList.remove('inactive');
-};
-
-// 프로필 수정 페이지로 이동
-const onProfileEdit = () => {
-	if (loginStatus.value === false) {
-		return router.push('/sign-in');
-	}
-	router.push(`/profile-edit`);
-}
-
-// 내 게시물 표시 상태
-const isUserBoardOn = ref(false);
-const onMyBoard = () => {
-	isUserBoardOn.value = true;
-	modalOpenClass();
-};
-const offUserBoard = () => {
-	isUserBoardOn.value = false;
-	modalCloseClass();
-};
-
-// 북마크 표시 상태
-const isBookmarkOn = ref(false);
-const onBookmark = () => {
-	isBookmarkOn.value = true;
-	modalOpenClass();
-};
-const offBookMark = () => {
-	isBookmarkOn.value = false;
-	modalCloseClass();
-};
-
-// 모달 상태
-const modalValue = ref(false);
-const modalText = ref('로그아웃 하시겠습니까?');
-const closeModal = () => {
-	modalValue.value = false;
-};
-
-const openEmailForm = () => {
-	const { userEmail, userNickname, userCountry } = userInfo || {}; // userInfo가 정의되지 않은 경우를 대비
-	const deviceInfo = navigator.userAgent;
-	const appVersion = '앱 버전';
-	const osVersionMatch = navigator.userAgent.match(/(?:\w+\s)?(?:\w+\s)?(?:\w+\/)?([\d._]+)/);
-	const osVersion = osVersionMatch ? osVersionMatch[1] : 'N/A';
-	const email = 'immilog@gmail.com';
-	const subject = '[immilog] 문의/요청/신고 합니다';
-	const body = `\n\n--------------------\n이메일: ${userEmail || 'N/A'}\n닉네임: ${userNickname || 'N/A'}\n접속국가: ${userCountry || 'N/A'}\n기기 정보: ${deviceInfo}\n앱 버전: ${appVersion}\nOS 버전: ${osVersion}`;
-	const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-
-	window.location.href = mailtoLink;
-};
-
-// 로그아웃 처리
-const signOut = () => {
-	userInfo.signOut();
-	removeTokens();
-	router.push('/sign-in');
-};
-
-const removeTokens = () => {
-	localStorage.removeItem('accessToken');
-	localStorage.removeItem('refreshToken');
-};
-
-// 사용자 정보 확인 후 로그인 페이지로 리다이렉트
-onMounted(async () => {
-	if (userInfo.userNickname) {
-		loginStatus.value = true;
+watchEffect(() => {
+	if (isKoreanChecked.value) {
+		locale.value = 'ko';
+	} else {
+		locale.value = 'en'; // 이 부분에서 원하는 언어 코드로 변경
 	}
 });
 
+// modal open/close 시 body 컨트롤
+const modalOpenClass = () => {
+  document.body.classList.add('inactive')
+}
+const modalCloseClass = () => {
+  document.body.classList.remove('inactive')
+}
+
+// 프로필 수정 페이지로 이동
+const onProfileEdit = () => {
+  if (loginStatus.value === false) {
+    return router.push('/sign-in')
+  }
+  router.push(`/profile-edit`)
+}
+
+// 내 게시물 표시 상태
+const isUserBoardOn = ref(false)
+const onMyBoard = () => {
+  isUserBoardOn.value = true
+  modalOpenClass()
+}
+const offUserBoard = () => {
+  isUserBoardOn.value = false
+  modalCloseClass()
+}
+
+// 북마크 표시 상태
+const isBookmarkOn = ref(false)
+const onBookmark = () => {
+  isBookmarkOn.value = true
+  modalOpenClass()
+}
+const offBookMark = () => {
+  isBookmarkOn.value = false
+  modalCloseClass()
+}
+
+// 모달 상태
+const modalValue = ref(false)
+const modalText = ref('로그아웃 하시겠습니까?')
+const closeModal = () => {
+  modalValue.value = false
+}
+
+const openEmailForm = () => {
+  const { userEmail, userNickname, userCountry } = userInfo || {} // userInfo가 정의되지 않은 경우를 대비
+  const deviceInfo = navigator.userAgent
+  const appVersion = '앱 버전'
+  const osVersionMatch = navigator.userAgent.match(/(?:\w+\s)?(?:\w+\s)?(?:\w+\/)?([\d._]+)/)
+  const osVersion = osVersionMatch ? osVersionMatch[1] : 'N/A'
+  const email = 'immilog@gmail.com'
+  const subject = '[immilog] 문의/요청/신고 합니다'
+  const body = `\n\n--------------------\n이메일: ${userEmail || 'N/A'}\n닉네임: ${userNickname || 'N/A'}\n접속국가: ${userCountry || 'N/A'}\n기기 정보: ${deviceInfo}\n앱 버전: ${appVersion}\nOS 버전: ${osVersion}`
+  const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
+
+  window.location.href = mailtoLink
+}
+
+// 로그아웃 처리
+const signOut = () => {
+  userInfo.signOut()
+  removeTokens()
+  router.push('/sign-in')
+}
+
+const removeTokens = () => {
+  localStorage.removeItem('accessToken')
+  localStorage.removeItem('refreshToken')
+}
+
+// 사용자 정보 확인 후 로그인 페이지로 리다이렉트
+onMounted(async () => {
+  if (userInfo.userNickname) {
+    loginStatus.value = true
+  }
+})
+
 // <-- 알림 팝업 관련
 
-const onMenu = ref(false);
+const onMenu = ref(false)
 
 const onMenuOpen = () => {
-	onMenu.value = true;
-};
+  onMenu.value = true
+}
 
 const onMenuClose = () => {
-	onMenu.value = false;
-};
+  onMenu.value = false
+}
 // -->
 </script>
